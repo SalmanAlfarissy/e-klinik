11cfe0648c1789440fb392c81a2a2a5e479389f2
SalmanAlfarissy <salman.alfarissy26@gmail.com>
2024-06-08 22:36:51 +0700

    feat(Pegawai) : Membuat fitur pegawai - 1

--- protected/controllers/PegawaiController.php
+++ protected/controllers/PegawaiController.php
@@ -1,6 +1,6 @@
 <?php
 
-class UserController extends Controller
+class PegawaiController extends Controller
 {
 	/**
 	 * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
@@ -27,8 +27,8 @@ class UserController extends Controller
 	public function accessRules()
 	{
 		return array(
-			array('allow', 
-				'actions'=>array('index','view','create','update','delete','admin'),
+			array('allow',
+				'actions'=>array('index','view','create','update','admin','delete'),
 				'roles'=>array('admin'),
 			),
 			array('deny',  // deny all users
@@ -54,14 +54,14 @@ class UserController extends Controller
 	 */
 	public function actionCreate()
 	{
-		$model=new User;
+		$model=new Pegawai;
 
 		// Uncomment the following line if AJAX validation is needed
 		// $this->performAjaxValidation($model);
 
-		if(isset($_POST['User']))
+		if(isset($_POST['Pegawai']))
 		{
-			$model->attributes=$_POST['User'];
+			$model->attributes=$_POST['Pegawai'];
 			if($model->save())
 				$this->redirect(array('view','id'=>$model->id));
 		}
@@ -83,9 +83,9 @@ class UserController extends Controller
 		// Uncomment the following line if AJAX validation is needed
 		// $this->performAjaxValidation($model);
 
-		if(isset($_POST['User']))
+		if(isset($_POST['Pegawai']))
 		{
-			$model->attributes=$_POST['User'];
+			$model->attributes=$_POST['Pegawai'];
 			if($model->save())
 				$this->redirect(array('view','id'=>$model->id));
 		}
@@ -114,7 +114,7 @@ class UserController extends Controller
 	 */
 	public function actionIndex()
 	{
-		$dataProvider=new CActiveDataProvider('User');
+		$dataProvider=new CActiveDataProvider('Pegawai');
 		$this->render('index',array(
 			'dataProvider'=>$dataProvider,
 		));
@@ -125,10 +125,10 @@ class UserController extends Controller
 	 */
 	public function actionAdmin()
 	{
-		$model=new User('search');
+		$model=new Pegawai('search');
 		$model->unsetAttributes();  // clear any default values
-		if(isset($_GET['User']))
-			$model->attributes=$_GET['User'];
+		if(isset($_GET['Pegawai']))
+			$model->attributes=$_GET['Pegawai'];
 
 		$this->render('admin',array(
 			'model'=>$model,
@@ -139,12 +139,12 @@ class UserController extends Controller
 	 * Returns the data model based on the primary key given in the GET variable.
 	 * If the data model is not found, an HTTP exception will be raised.
 	 * @param integer $id the ID of the model to be loaded
-	 * @return User the loaded model
+	 * @return Pegawai the loaded model
 	 * @throws CHttpException
 	 */
 	public function loadModel($id)
 	{
-		$model=User::model()->findByPk($id);
+		$model=Pegawai::model()->findByPk($id);
 		if($model===null)
 			throw new CHttpException(404,'The requested page does not exist.');
 		return $model;
@@ -152,11 +152,11 @@ class UserController extends Controller
 
 	/**
 	 * Performs the AJAX validation.
-	 * @param User $model the model to be validated
+	 * @param Pegawai $model the model to be validated
 	 */
 	protected function performAjaxValidation($model)
 	{
-		if(isset($_POST['ajax']) && $_POST['ajax']==='user-form')
+		if(isset($_POST['ajax']) && $_POST['ajax']==='pegawai-form')
 		{
 			echo CActiveForm::validate($model);
 			Yii::app()->end();
