c479ebf7e067073000095baa83a9623833a12a67
SalmanAlfarissy <salman.alfarissy26@gmail.com>
2024-06-08 23:10:06 +0700

    feat(Obat) : membuat fitur obat - 1

---
+++ protected/views/obat/_view.php
@@ -0,0 +1,33 @@
+<?php
+/* @var $this ObatController */
+/* @var $data Obat */
+?>
+
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('nama')); ?>:</b>
+	<?php echo CHtml::encode($data->nama); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('deskripsi')); ?>:</b>
+	<?php echo CHtml::encode($data->deskripsi); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('harga')); ?>:</b>
+	<?php echo CHtml::encode($data->harga); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('created_at')); ?>:</b>
+	<?php echo CHtml::encode($data->created_at); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('updated_at')); ?>:</b>
+	<?php echo CHtml::encode($data->updated_at); ?>
+	<br />
+
+
+</div>
